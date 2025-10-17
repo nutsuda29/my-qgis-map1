62243fb980aa5d794c40a941795f04fa278e1755
nutsuda29 <nutsuda.k@kkumail.com>
2025-10-17 14:06:01 +0700

    Add files via upload

---
+++ qgis2web_2025_10_17-14_01_34_532307/layers/layers.js
@@ -0,0 +1,163 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_2_1 = new ol.format.GeoJSON();
+var features_2_1 = format_2_1.readFeatures(json_2_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_2_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_2_1.addFeatures(features_2_1);
+var lyr_2_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_2_1, 
+                style: style_2_1,
+                popuplayertitle: 'รวมทุกอย่าง เสาไฟฟ้าส่องสว่างน้องใบเตย (#2)',
+                interactive: true,
+                title: '<img src="styles/legend/2_1.png" /> รวมทุกอย่าง เสาไฟฟ้าส่องสว่างน้องใบเตย (#2)'
+            });
+var format__2 = new ol.format.GeoJSON();
+var features__2 = format__2.readFeatures(json__2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__2.addFeatures(features__2);
+var lyr__2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__2, 
+                style: style__2,
+                popuplayertitle: 'รวมทุกอย่าง เสาไฟฟ้าส่องสว่างน้องโบ',
+                interactive: true,
+                title: '<img src="styles/legend/_2.png" /> รวมทุกอย่าง เสาไฟฟ้าส่องสว่างน้องโบ'
+            });
+var format__3 = new ol.format.GeoJSON();
+var features__3 = format__3.readFeatures(json__3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__3.addFeatures(features__3);
+var lyr__3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__3, 
+                style: style__3,
+                popuplayertitle: 'รวมทุกอย่าง เสาไฟฟ้าส่องสว่างน้องโด',
+                interactive: true,
+                title: '<img src="styles/legend/_3.png" /> รวมทุกอย่าง เสาไฟฟ้าส่องสว่างน้องโด'
+            });
+var format__4 = new ol.format.GeoJSON();
+var features__4 = format__4.readFeatures(json__4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__4.addFeatures(features__4);
+var lyr__4 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__4, 
+                style: style__4,
+                popuplayertitle: 'รวมทุกอย่าง เสาไฟฟ้าส่องสว่างของไนซ์',
+                interactive: true,
+                title: '<img src="styles/legend/_4.png" /> รวมทุกอย่าง เสาไฟฟ้าส่องสว่างของไนซ์'
+            });
+var format__5 = new ol.format.GeoJSON();
+var features__5 = format__5.readFeatures(json__5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__5.addFeatures(features__5);
+var lyr__5 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__5, 
+                style: style__5,
+                popuplayertitle: 'รวมทุกอย่าง เสาไฟฟ้าส่องสว่าง.เอิร์ท',
+                interactive: true,
+                title: '<img src="styles/legend/_5.png" /> รวมทุกอย่าง เสาไฟฟ้าส่องสว่าง.เอิร์ท'
+            });
+var format__6 = new ol.format.GeoJSON();
+var features__6 = format__6.readFeatures(json__6, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__6 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__6.addFeatures(features__6);
+var lyr__6 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__6, 
+                style: style__6,
+                popuplayertitle: 'รวมทุกอย่าง เนินชะลอความเร็ว/หลุม/บ่อ/ถนนชำรุด',
+                interactive: true,
+                title: '<img src="styles/legend/_6.png" /> รวมทุกอย่าง เนินชะลอความเร็ว/หลุม/บ่อ/ถนนชำรุด'
+            });
+var format__7 = new ol.format.GeoJSON();
+var features__7 = format__7.readFeatures(json__7, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource__7 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource__7.addFeatures(features__7);
+var lyr__7 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource__7, 
+                style: style__7,
+                popuplayertitle: 'รวมทุกอย่าง จุดทิ้งขยะและถังขยะ',
+                interactive: true,
+                title: '<img src="styles/legend/_7.png" /> รวมทุกอย่าง จุดทิ้งขยะและถังขยะ'
+            });
+var format_CopyofLayer1_8 = new ol.format.GeoJSON();
+var features_CopyofLayer1_8 = format_CopyofLayer1_8.readFeatures(json_CopyofLayer1_8, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_CopyofLayer1_8 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_CopyofLayer1_8.addFeatures(features_CopyofLayer1_8);
+var lyr_CopyofLayer1_8 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_CopyofLayer1_8, 
+                style: style_CopyofLayer1_8,
+                popuplayertitle: 'รวมทุกอย่าง Copy of Layer 1',
+                interactive: true,
+                title: '<img src="styles/legend/CopyofLayer1_8.png" /> รวมทุกอย่าง Copy of Layer 1'
+            });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_2_1.setVisible(true);lyr__2.setVisible(true);lyr__3.setVisible(true);lyr__4.setVisible(true);lyr__5.setVisible(true);lyr__6.setVisible(true);lyr__7.setVisible(true);lyr_CopyofLayer1_8.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_2_1,lyr__2,lyr__3,lyr__4,lyr__5,lyr__6,lyr__7,lyr_CopyofLayer1_8];
+lyr_2_1.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__2.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__3.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__4.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__5.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__6.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr__7.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_CopyofLayer1_8.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_2_1.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr__2.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr__3.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr__4.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr__5.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr__6.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr__7.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_CopyofLayer1_8.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_2_1.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr__2.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr__3.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr__4.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr__5.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr__6.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr__7.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_CopyofLayer1_8.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_CopyofLayer1_8.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
